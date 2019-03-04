0674a51972448135901630d695bab5b2457a202b
chengrq <845839014@qq.com>
2019-03-04 15:55:52 +0800

    first

--- src/main/java/com/nowcoder/toutiao/controller/IndexController.java
+++ src/main/java/com/nowcoder/toutiao/controller/IndexController.java
@@ -2,7 +2,9 @@ package com.nowcoder.toutiao.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,5 +15,20 @@ public class IndexController {
     @ResponseBody
     public String index() {
         return "hello";
+    }
+
+    @RequestMapping(value = {"/profile/{groupId}/{userId}"})
+    @ResponseBody
+    public String profile(@PathVariable("groupId") String groupId,
+                          @PathVariable("userId") int userId,
+                          @RequestParam(value = "type",defaultValue = "1") int type,
+                          @RequestParam(value = "key", defaultValue = "nowcoder") String key){
+        return String.format("GID{%s},UID{%d},TYPE{%d},KEY{%s}", groupId,userId,type,key);
+    }
+
+    @RequestMapping(value = {"/ftl"})
+    public String news(){
+        //model.addAttribute("value1", "vv1");
+        return "news";
     }
 }
---
+++ src/main/resources/templates/news.ftl
@@ -0,0 +1,7 @@
+<html>
+<body>
+<pre>
+Hello FTL.
+</pre>
+</body>
+</html>
